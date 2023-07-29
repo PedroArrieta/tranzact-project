a2cf5e59afeb9e3e97f8de9f190f06423ca6a9cb
Pedro Arrieta <pedroarrieta43@gmail.com>
2023-07-29 11:27:15 -0500

    updating network security rule

--- scripts/tf-networking/main.tf
+++ scripts/tf-networking/main.tf
@@ -24,17 +24,17 @@ resource "azurerm_network_security_group" "network_security_group" {
 }
 
 resource "azurerm_network_security_rule" "network_security_rule" {
-    name                        = "test123"
-    priority                    = 100
-    direction                   = "Outbound"
-    access                      = "Allow"
-    protocol                    = "Tcp"
-    source_port_range           = "*"
-    destination_port_range      = "*"
-    source_address_prefix       = "*"
-    destination_address_prefix  = "*"
-    resource_group_name         = "${azurerm_resource_group.example.name}"
-    network_security_group_name = "${azurerm_network_security_group.example.name}"
+    name                        = var.network_security_rule_name
+    priority                    = var.priority
+    direction                   = var.direction
+    access                      = var.access
+    protocol                    = var.protocol
+    source_port_range           = var.source_port_range
+    destination_port_range      = var.destination_port_range
+    source_address_prefix       = var.source_address_prefix
+    destination_address_prefix  = var.destination_address_prefix
+    resource_group_name         = var.resource_group_name
+    network_security_group_name = azurerm_network_security_group.network_security_group.name
 }
 
 resource "azurerm_public_ip" "public_ip" {
